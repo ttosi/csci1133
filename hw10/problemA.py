f148bae50c4da49ffee14153316e07ace5aa8bff
tony.tosi <tony.tosi@pima.gov>
2023-03-30 10:29:38 -0700

    hw10 problem A & B

--- hw10/problemA.py
+++ hw10/problemA.py
@@ -1,27 +1,19 @@
 def find_the_mins(dictionary):
-  new_dict = {}
-  for item in dictionary:
-    if len(dictionary[item]) > 0:
-      new_dict[item] = min(dictionary[item])
-  
-  return new_dict
+    new_dict = {}
+    for item in dictionary:
+        if len(dictionary[item]) > 0:
+            new_dict[item] = min(dictionary[item])
+
+    return new_dict
+
 
 if __name__ == "__main__":
-   print(find_the_mins({
-    "One": [1, 2, 3],
-    "Two": [4, 5, 6],
-    "Three": [44, 41, 41]
-  }))
+    print(find_the_mins({"One": [1, 2, 3], "Two": [4, 5, 6], "Three": [44, 41, 41]}))
+    print(find_the_mins({"X": [-1, 3, -33, 100], "Y": [], "Z": [1, 1, 0], "W": [3]}))
 
-   print(find_the_mins({
-    "X": [-1, 3, -33, 100],
-    "Y": [], "Z": [1, 1, 0],
-    "W": [3]
-   }))
-
-   print(find_the_mins({
-    "In": [12, 100, 72, 74, 11, 89],
-    "Out": [14, 100],
-    "Failed": [-1, 0, 0, 1],
-    "Queued": [3],
-    }))
+#  print(find_the_mins({
+#   "In": [12, 100, 72, 74, 11, 89],
+#   "Out": [14, 100],
+#   "Failed": [-1, 0, 0, 1],
+#   "Queued": [3],
+#   }))
--- hw10/problemB.py
+++ hw10/problemB.py
@@ -1,32 +1,33 @@
 def find_the_contact(directory, name, field):
-  for user in directory:
-    if name == user:
-      return directory[user][field]
-    
-    try:
-      return directory[name]
-    except KeyError:
-      return name[:3]
+    for user in directory:
+        if name == user:
+            return directory[user][field]
 
-
-
+        try:
+            return directory[name]
+        except KeyError:
+            return name[:3]
 
 
 if __name__ == "__main__":
-  # a_dict = {
-  #   'Lee': {'Phone': '643-756-5612', 'Email': 'example@umn.edu', 'Username': 'Lee'},
-  #   'Katie': {'Email': 'test_email@gmail.com', 'Username': 'Kat'},
-  #   'Amanda': {'Phone': '234-462-4513', 'Email':'no_email@yahoo.com',  'Username': 'Ama'}
-  # }
-  # print(find_the_contact(a_dict, "Lee", "Phone"))
-  # print(find_the_contact(a_dict, "Nat", "Email"))
-  # print(find_the_contact(a_dict, "Abi", "Email"))
+    a_dict = {
+        "Lee": {"Phone": "643-756-5612", "Email": "example@umn.edu", "Username": "Lee"},
+        "Katie": {"Email": "test_email@gmail.com", "Username": "Kat"},
+        "Amanda": {
+            "Phone": "234-462-4513",
+            "Email": "no_email@yahoo.com",
+            "Username": "Ama",
+        },
+    }
+    print(find_the_contact(a_dict, "Lee", "Phone"))
+    print(find_the_contact(a_dict, "Nat", "Email"))
+    print(find_the_contact(a_dict, "Abi", "Email"))
 
-  a_dict = {
-    'Lee': {'Phone': '643-756-5612', 'Email': 'example@umn.edu', 'Username': 'Lee'}, 
-    'Katie': {'Email': 'test_email@gmail.com', 'Username': 'Kat'},
-    'Amanda': {'Phone': '234-462-4513', 'Email': 'no_email@yahoo.com', 'Username': 'Ama'}, 
-    'Nat': {'Phone': '612-379-5234', 'Username': 'Nat'}, 
-    'Abi': {'Username': 'Abi'}
-  }
-  print(find_the_contact(a_dict, "Abby", "Username"))
+    # a_dict = {
+    #   'Lee': {'Phone': '643-756-5612', 'Email': 'example@umn.edu', 'Username': 'Lee'},
+    #   'Katie': {'Email': 'test_email@gmail.com', 'Username': 'Kat'},
+    #   'Amanda': {'Phone': '234-462-4513', 'Email': 'no_email@yahoo.com', 'Username': 'Ama'},
+    #   'Nat': {'Phone': '612-379-5234', 'Username': 'Nat'},
+    #   'Abi': {'Username': 'Abi'}
+    # }
+    # print(find_the_contact(a_dict, "Abby", "Username"))
